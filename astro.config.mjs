7480b97bbb781af4ffd3726610f983171a07cfdc
Han Lin <hotdogee@gmail.com>
2025-05-11 06:55:58 +0800

    feat: Add Partytown integration
    Integrates Partytown to offload third-party scripts.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 import mdx from '@astrojs/mdx';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hanl.in',
@@ -22,5 +24,5 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 
-  integrations: [mdx()],
+  integrations: [mdx(), partytown()],
 });
