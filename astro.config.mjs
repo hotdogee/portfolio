3da117ab8cbb947500eb9761330f1748b32295d7
Han Lin <hotdogee@gmail.com>
2025-05-11 07:28:08 +0800

    feat: Configure markdown syntax highlighting
    Adds Shiki for markdown syntax highlighting.
    Configures light and dark themes, a language alias for cjs, and enables word wrap.

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,4 +25,30 @@ export default defineConfig({
   },
 
   integrations: [mdx(), partytown()],
+  markdown: {
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://shiki.style/themes
+      themes: {
+        light: 'one-light',
+        dark: 'one-dark-pro',
+      },
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://shiki.style/languages
+      langs: [],
+      // Add custom aliases for languages
+      // Map an alias to a Shiki language ID: https://shiki.style/languages#bundled-languages
+      // https://shiki.style/guide/load-lang#custom-language-aliases
+      langAlias: {
+        cjs: 'javascript',
+      },
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+      // Add custom transformers: https://shiki.style/guide/transformers
+      // Find common transformers: https://shiki.style/packages/transformers
+      transformers: [],
+    },
+  },
 });
