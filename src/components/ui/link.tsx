5fc4635f65dc1c82472f7defe70cf91031cd7c5c
Han Lin <hotdogee@gmail.com>
2025-05-12 18:57:35 +0800

    feat(ui): add Button, Card, and Link components

---
+++ src/components/ui/link.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Button } from './button'
+
+export interface LinkProps extends React.ComponentProps<typeof Button> {
+  href: string
+  children: React.ReactNode
+}
+
+const Link: React.FC<LinkProps> = ({ href, children, ...props }) => {
+  return (
+    <Button {...props} asChild>
+      <a href={href}>{children}</a>
+    </Button>
+  )
+}
+
+export { Link }
